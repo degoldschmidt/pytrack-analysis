2255b75622f64b3f135eb8eda608fb774f64a5f5
degoldschmidt <goldschmidt.de@gmail.com>
2017-07-25 17:59:12 +0200

    New structure

--- pytrack_analysis/database.py
+++ pytrack_analysis/database.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import numpy as np
 import pandas as pd
 from . import data_integrity
@@ -6,6 +6,11 @@ import yaml
 import json
 from collections.abc import Mapping
 from asciitree import draw_tree
+import time
+
+"""
+DataFileFormats IO
+"""
 
 def json2dict(_file):
     """Convert JSON to dict"""
@@ -28,6 +33,109 @@ def yaml2dict(val):
         return yaml.safe_load_all(val)
     except yaml.YAMLError as exc:
         return exc
+
+"""
+Data Integrity Tests
+"""
+
+def get_created(filepath):
+    """
+    Returns created timestamp for given filepath
+
+    args:
+    * filepath [str] : full path of given file
+    """
+    try:
+        return time.ctime(os.path.getctime(filepath))
+    except OSError:
+        return 0
+
+def get_modified(filepath):
+    """
+    Returns last modified timestamp for given filepath
+
+    args:
+    * filepath [str] : full path of given file
+    """
+    try:
+        return time.ctime(os.path.getmtime(filepath))
+    except OSError:
+        return 0
+
+def check_base(_dict, _dir):
+    """
+    Checks and returns boolean, whether database is in given directory
+    """
+    for key in _dict.keys():
+        print("[O.K.]" if key in os.listdir(_dir) else "[FAILED]")
+    return (key in os.listdir(_dir))
+
+def check_meta(_dict, _dir):
+    """
+    Checks and returns boolean, whether all meta-data files are in given directory
+    """
+    flag = 0
+    for key,val in _dict.items():
+        for expkeys in val.keys():
+            if not expkeys in os.listdir(_dir):
+                flag = 1
+    print("[O.K.]" if flag == 0 else "[FAILED]")
+    return (flag == 0)
+
+def check_data(_dict, _dir):
+    """
+    Checks and returns boolean, whether all data files are in given directory
+    """
+    flag = 0
+    for key,val in _dict.items():
+        for expkeys, v2 in val.items():
+            for session in v2:
+                if not session in os.listdir(_dir):
+                    flag = 1
+    print("[O.K.]" if flag == 0 else "[FAILED]")
+    return (flag == 0)
+
+def check_time(_tstamps, _dir):
+    """
+    Checks and returns boolean, whether all files have valid timestamps
+    """
+    flag = 0
+    for filename, times in _tstamps.items():
+        mod = times["modified"]
+        cre = times["created"]
+        if not cre == get_created(os.path.join(_dir, filename)) and mod == get_modified(os.path.join(_dir, filename)):
+            flag = 1
+    print("[O.K.]" if flag == 0 else "[FAILED]")
+    return (flag == 0)
+
+def test(_dir, _dict, _tstamps, _VERBOSE=True):
+    """
+    Returns flags of several data integrity tests for given file structure from database file
+    """
+    if _VERBOSE:
+        sys.stdout = sys.__stdout__
+    else:
+        sys.stdout = open(os.devnull, 'w')
+
+    print("STARTING DATA INTEGRITY TEST...")
+    print("-------------------------------")
+    print("CHECKING DATABASE...\t\t\t", end='')
+    basefl = check_base(_dict, _dir)
+    print("CHECKING METAFILES...\t\t\t", end='')
+    metafl = check_meta(_dict, _dir)
+    print("CHECKING DATAFILES...\t\t\t", end='')
+    datafl = check_data(_dict, _dir)
+    print("CHECKING TIMESTAMPS...\t\t\t", end='')
+    timefl = check_time(_tstamps, _dir)
+    timefl = 0
+
+    sys.stdout = sys.__stdout__
+    return [basefl, metafl, datafl, timefl]
+
+
+"""
+GraphDict Implementation
+"""
 
 class Node(object):
     """ Node object for GraphDict """
@@ -84,6 +192,10 @@ class GraphDict(Mapping):
         return self[name]
 
 
+"""
+Database Implementation
+"""
+
 class Database(object):
     """
     Database class: contains meta-data for experiment database
@@ -121,7 +233,7 @@ class Database(object):
 
     def find(self, eqs):
         """
-        Function evaluates eqs to find given key-value match and returns session name 
+        Function evaluates eqs to find given key-value match and returns session name
         """
         for alleq in eqs:
             key = eqs.split("=")[0]
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="pytrack_analysis",
-    version="0.0.1.2",
+    version="0.0.2",
     author="Dennis Goldschmidt",
     author_email="dennis.goldschmidt@neuro.fchampalimaud.org",
     description=("Toolbox for analyzing object centroid data with Python."),
