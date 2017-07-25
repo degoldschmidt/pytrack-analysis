283b44b43929273f3e07a9c667990a59c8b338de
degoldschmidt <goldschmidt.de@gmail.com>
2017-07-25 17:30:24 +0200

    Minor changes to tests

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -8,3 +8,6 @@ if __name__ == '__main__':
     # load 'Vero eLife 2016' as user 'degoldschmidt'
     PROFILE = get_profile('Vero eLife 2016', 'degoldschmidt', script=thisscript)
     DB = Database(get_db(PROFILE)) # database from file
+    print("0003A01R01Cam03.avi is in:", DB.find('Videofilename=0003A01R01Cam03.avi'))
+    this_session = DB.experiment("CANS").session("005")
+    print(this_session.keys())
