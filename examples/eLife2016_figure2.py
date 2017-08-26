1d9289edc618bcfec6d6da3559f91049fda72a56
degoldschmidt <goldschmidt.de@gmail.com>
2017-08-26 16:47:09 +0100

    smaller changes

--- examples/eLife2016_figure2.py
+++ examples/eLife2016_figure2.py
@@ -3,7 +3,6 @@
 # date: 25/8/2017
 import logging
 import os
-import pytrack_analysis
 from pytrack_analysis.profile import *
 from pytrack_analysis.database import *
 from pytrack_analysis.logger import Logger
--- pytrack_analysis/__init__.py
+++ pytrack_analysis/__init__.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 # author: degoldschmidt
 # date: 25/8/2017
-from __future__ import absolute_import
-
 __version__     = "0.0.3"
 __all__         = ["benchmark", "database", "kinematics", "logger", "preprocessing", "profile", "settings", "statistics"]
-
 from .benchmark import Benchmark, Multibench
 from .kinematics import Kinematics
 from .logger import Logger
