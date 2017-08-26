52afe2d46cfa1aa927e940b65eefcd5a8366e60e
degoldschmidt <goldschmidt.de@gmail.com>
2017-08-26 16:37:21 +0100

    clean __init__

--- pytrack_analysis/__init__.py
+++ pytrack_analysis/__init__.py
@@ -3,11 +3,7 @@
 # date: 25/8/2017
 from __future__ import absolute_import
 
-__url__             = "https://pypi.python.org/pypi/pytrack-analysis"
 __version__         = "0.0.3"
-__license__         = "GPLv3"
-__author__          = "Dennis Goldschmidt"
-__author_email__    = "dennis.goldschmidt@neuro.fchampalimaud.org"
 __all__ = ["benchmark", "database", "kinematics", "logger", "preprocessing", "profile", "settings", "statistics"]
 
 from .benchmark import Benchmark, Multibench
