46c561fb9c3ef7b2c62d99e6090cfde6e02753c9
degoldschmidt <goldschmidt.de@gmail.com>
2017-11-20 14:41:54 +0000

    new metapacker

---
+++ pytrack_analysis/posttracking.py
@@ -0,0 +1,19 @@
+
+
+def check_distance(_X):
+    pass
+
+def detect_jumps(_X):
+    pass
+
+def check_orient(_X):
+    pass
+
+def flip_orient(_X):
+    pass
+
+def validate_all(_group):
+    pass
+
+def write_validated(_X):
+    pass
