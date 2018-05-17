bfc53b57a75f5434ee3b2b0769dbfcd533b6c858
Dennis Goldschmidt <goldschmidt.de@gmail.com>
2018-05-17 16:39:35 +0100

    minor fix

--- pytrack_analysis/__init__.py
+++ pytrack_analysis/__init__.py
@@ -2,7 +2,7 @@
 # author: degoldschmidt
 # date: 25/8/2017
 __version__     = "0.0.3"
-__all__         = ["benchmark", "database", "kinematics", "logger", "preprocessing", "profile", "settings", "statistics"]
+__all__         = ["benchmark", "database", "kinematics", "preprocessing", "profile", "settings", "statistics"]
 from .benchmark import Benchmark, Multibench
 from .node import Node
 from .classifier import Classifier
